0ed0d57e26c06d678bf1c67a1aa985acc9d28e82
Desmond Campbell <docampbell@gmail.com>
2020-01-15 11:35:58 -0500

    Posted updates to stack variable via model transform

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -9,232 +9,149 @@ use Illuminate\Support\Facades\Storage;
 
 class NotesController extends \App\Http\Controllers\Controller
 {
-    
-    public function store(Request $r){
 
+    public function store(Request $r): Object{
         $data = $r->data;
-
         $params = $r->params;
         $files = $r->params['files'] ?? [];
         $user_id = $params['user_id'] ?? 0;
-
         $id = $data['_id'] ?? null;
         $folder_id = $data['folder_id'] ?? null;
         $title = $data['title'] ?? '';
         $tags = $data['tags'] ?? '';
         $stack = $data['stack'] ?? [];
-        $content = implode( "\n\n", $stack ); 
+        $content = implode( "\n\n", $stack );
         $is_private = $data['is_private'] ?? null;
         $access_level = $data['access_level'] ?? null;
-
         if ( $id ) {
-
             $note = Note::where('user_id', $user_id)->find( $id );
-
             if ( $folder_id ) { $note->folder_id = $folder_id; }
             if ( $title ) { $note->title = $title; }
-            if ( $content ) { 
-                $note->content = $content; 
+            if ( $content ) {
+                $note->content = $content;
                 $note->stack = $stack;
             }
             if ( $tags ) { $note->tags = $tags; }
             if ( $access_level ) { $note->access_level = $access_level; }
             if ( !is_null( $is_private ) ) { $note->is_private = $is_private; }
-        
-            $note->keywords = implode( ' ', Note::words( $content . ' ' . $tags . ' ' . $title ) ); 
-
+            $note->keywords = implode( ' ', Note::words( $content . ' ' . $tags . ' ' . $title ) );
             $note->save();
-
         } else {
-
             if ( !$folder_id ) {
-
                 $folder = Folder::where('user_id', $user_id)->where('is_default', 1)->first();
-
                 if ( !$folder ) {
-
                     $folder = Folder::getDefaultFolder($user_id);
-
                 }
-
                 $folder_id = $folder->id;
-
             }
-
             if ( !$title ) {
-
                 $title = 'Random Note - ' . date('F d, H:i a');
-
             }
-
             $note_data = [];
             $note_data['folder_id'] = $folder_id;
             $note_data['title'] = $title;
             $note_data['content'] = $content;
-            $note_data['stack'] = $stack; 
-            $note_data['keywords'] = implode( ' ', Note::words( $content . ' ' . $tags . ' ' . $title ) ); 
+            $note_data['stack'] = $stack;
+            $note_data['keywords'] = implode( ' ', Note::words( $content . ' ' . $tags . ' ' . $title ) );
             $note_data['tags'] = $tags;
             $note_data['is_private'] = $is_private ?? false;
             $note_data['access_level'] = $access_level ?? 0;
             $note_data['user_id'] = $user_id;
-
             $note = Note::create( $note_data );
-
         }
-
         return response()->json( [ 'note' => $note, 'success' => true ] );
-
     }
 
-    public function delete(Request $r){
-
+    public function delete(Request $r): Object {
         $user_id = $r->user_id;
         $id = $r->_id;
-
         $note = Note::where('user_id', $user_id)->find($id);
-
         if ( $note ) {
-
             $note->delete();
-
             return response()->json( [ 'success' => true, 'note' => [ 'title' => 'Untitled Note', '_id' => null, 'message' => 'Original requested note not found. This is a new note.', 'stack' => [ '' ] ] ] );
-
         }
-
         return response()->json( [ 'success' => false ] );
-
     }
 
-    public function get(Request $r){
-
+    public function get(Request $r): Object {
         $user_id = $r->user_id;
-       
         $id = $r->_id;
-
         $note = Note::where('user_id', $user_id)->find($id);
-
         if ( !$note ) {
             $note = [ 'title' => 'Untitled Note', '_id' => null, 'message' => 'Original requested note not found. This is a new note.', 'stack' => [ '' ] ];
+        } else {
+            $note = Note::transform($note->stack);
         }
-
         return response()->json( $note );
-
     }
 
-    public function browse(Request $r){
-
+    public function browse(Request $r): Object {
         $user_id = $r->user_id;
-        
         $paging = $r->input('paging') ?? [ 'currentPage' => 1, 'limit' => 15 ];
-
         $args = [ 'paging' => $paging, 'user_id' => $user_id ];
-
         $result = Note::fetch( $args );
-
         return response()->json( $result );
-
     }
 
-    public function storeFolder(Request $r){
-
+    public function storeFolder(Request $r): Object {
         $data = $r->data;
-
         $params = $r->params;
-
         $user_id = $params['user_id'];
-      
         $id = $data['_id'] ?? null;
         $name = $data['name'] ?? '';
-
         if ( $id ) {
-
             $folder = Folder::where('user_id', $user_id)->find( $id );
-            
             if ( $name ) $folder->name = $name;
-            
             $folder->save();
-
         } else {
-
             $folder = [];
             $folder['name'] = $name;
             $folder['user_id'] = $user_id;
-
             $folder = Folder::create( $folder );
-
         }
-
         $folders = Folder::fetch( [ 'user_id' => $user_id ] );
-
         return response()->json( [ 'folder' => $folder, 'success' => true, 'folders' => $folders ] );
-
     }
 
-    public function deleteFolder(Request $r){
-
+    public function deleteFolder(Request $r): Object {
         $id = $r->_id;
         $user_id = $r->user_id;
-
         $folder = Folder::where('user_id', $user_id)->find($id);
-
         if ( $folder ) {
-
             if ( $folder->is_default ) {
                 return response()->json( [ 'errors' => 'Cannot delete this folder as it is set as the default folder.' ] );
             }
-
             $notes = Note::where('folder_id', $folder->id)->get();
-
             if ( $notes ) {
-
                 foreach ( $notes as $note ) {
                     $note->folder_id = Folder::getDefaultFolder($user_id)->id;
                     $note->save();
                 }
-
             }
-
             $folder->delete();
-
             $folders = Folder::fetch( [ 'user_id' => $user_id ] );
-
             return response()->json( [ 'success' => true, 'folders' => $folders ] );
-
         }
-
         return response()->json( [ 'success' => false ] );
-
     }
 
-    public function getFolders(Request $r){
-
+    public function getFolders(Request $r): Object {
         $user_id = $r->user_id;
-    
         $paging = $r->input('paging') ?? [ 'page' => 1, 'limit' => 15 ];
-
         $args = [ 'paging' => $paging, 'user_id' => $user_id ];
-
         $result = Folder::fetch( $args );
-
         return response()->json( $result );
-
     }
 
-    private function str_random( $length ) {
-
+    private function str_random( $length ): String {
         $chars = str_split( 'qwePSDFHBGEWVLSUIYUHJKLKMLQEJIH2JUTGKJMRLBKJNlhgfyglYFSMVLKLAJWKHJKKLQI4HJTLKlkvgrwhjfmewovinhybiefnvodmwfnbhnvwjdfvbnieifvjGRMDVLBKDJGLMDVASCVMSBNKJEWLDWNIHETJGNRKMEWFLOPIRETHJEGKRLDFVKBJVDLSCrtyuiopasdfghjlklanbfvgubhejnLHGVJHBIIFEH34IPTPLEEJNKBHDWFVGJMLIBHYUVJKWCEWIUEBHEFKJWDVOFBIOJGNDFLKSDMVLLKDFJBKGSVDlkvuishyukejtjiwhrgsd', 1 );
         $n = count( $chars );
         $string = '';
-
         for ( $i = 0; $i < $length; $i++ ) {
-
             $r = rand(0, $n-1);
             $string .= $chars[$r];
-
         }
-
         return $string;
-
     }
 
 }
--- app/Note.php
+++ app/Note.php
@@ -8,37 +8,28 @@ use App\Services\Http;
 
 class Note extends Model
 {
-    
+
     protected $guarded = [ 'id' ];
 
-    public static function fetch( $args ) {
-
+    public static function fetch( array $args ): array {
 	  	$user_id = $args['user_id'] ?? 0;
-
         $notes = self::where( 'user_id', $user_id );
-
         if ( $args['paging']['folder_id'] ?? null ) $notes = $notes->where('folder_id', $args['paging']['folder_id']);
         // if ( !( $args['show_private'] ?? false ) ) $notes = $notes->where('is_private', 0);
-
         $limit = (int) ( $args['paging']['limit'] ?? 15 );
         $page = (int) ( $args['paging']['currentPage'] ?? 1 );
-
         $sortIcons = $args['paging']['sortIcons'] ?? [];
         $sortField = $args['paging']['sortField'] ?? 'created_at';
         $sortOrder = $args['paging']['sortOrder'] ?? 'desc';
-
         $notes = $notes->orderBy( $sortField, $sortOrder );
-
         $keywords = $args['paging']['keywords'] ?? [];
 
         if ( $keywords ) {
-
-            $notes = $notes->where(function($q) use ($keywords) { 
+            $notes = $notes->where(function($q) use ($keywords) {
               $q->where('title', 'like', '%' . $keywords . '%');
               $q->orWhere('keywords', 'like', '%' . $keywords . '%');
               $q->orWhere('tags', 'like', '%' . $keywords . '%');
             });
-
         }
 
         $notes = $notes->paginate( $limit, ['*'], null, $page );
@@ -49,26 +40,34 @@ class Note extends Model
         }
 
         $notes = $notes->items();
-
-        $result = [ 'notes' => $notes ];
-
+        $result = [ 'notes' => static::transformAll( $notes ) ];
         $result['paging'] = $paging;
 
         return $result;
 
     }
 
-    public static function words( $string ) {
-
+    public static function words( $string ): array {
         $string = strip_tags( $string );
         $string = strtolower( $string );
         $words = explode( ' ', $string );
         $words = array_map( 'trim', $words );
         $words = array_unique( $words );
         $words = array_filter( $words );
-
         return $words;
+    }
 
+    public static function transformAll(array $entities): array {
+        for($i = 0; $i < count( $entities ); $i++) {
+            $entity = static::transform($entities[$i]);
+            $entities[$i] = $entity;
+        }
+        return $entities;
+    }
+
+    public static function transform(Object $entity): Object {
+        $entity->stack = array_values( $entity->stack ?? [] );
+        return $entity;
     }
 
 }
